632d861b053ca79f72c5c6ac6c3ff32d7c27d9ef
xiaozhang <954922386@qq.com>
2025-08-26 10:36:26 +0800

    change print

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ class MyPlugin(Star):
         message_str = event.message_str # 用户发的纯文本消息字符串
         message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
         logger.info(message_chain)
-        yield event.plain_result(f"好了好了, {message_str}被{user_name}草饲了。") # 发送一条纯文本消息
+        user = message_str[3:]
+        yield event.plain_result(f"好了好了, {user}被{user_name}草饲了。") # 发送一条纯文本消息
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
